67a307f81cdbf7aed4d3856f706443e4e773eb91
serzavdmit <118341093+serzavdmit@users.noreply.github.com>
2022-12-13 19:45:39 +0200

    Add files via upload

---
+++ обчислення функції/обчислення функції/обчислення функції.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Ukr");
+	double x, y, min, max;
+	cout << "Введiть значення х= ";
+	cin >> x;
+	cout << "Введiть значення y=";
+	cin >> y;
+	{
+	if (x <= pow((x + y), 2)) min = x;
+	else min = pow((x + y), 2);
+	}
+	{
+		if (x >= pow(y, 3)) max = y;
+		else max = pow(y, 3);
+	}
+	cout << "F= " << min / ((pow(x, 2) + max));
+}
+
